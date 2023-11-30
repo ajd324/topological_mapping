1d3208af34715c78543129d01a8e792456a4eb41
ajd324 <148496987+ajd324@users.noreply.github.com>
2023-11-30 15:30:32 -0500

    Update graph_mapping_final.py
    added depth camera data to nodes

--- graph_mapping_final.py
+++ graph_mapping_final.py
@@ -11,7 +11,7 @@ import rosbag
 x_pose = 0
 y_pose = 0
 bridge = CvBridge()
-odom_bag = rosbag.Bag('/home/agilex/topological_mapping/bag_files/odometry.bag','w')
+odom_bag = rosbag.Bag('/home/administrator/mapping/rosbags/odometry.bag','w')
 
 def odom_callback(odom_msg):
     global x_pose
@@ -21,13 +21,24 @@ def odom_callback(odom_msg):
     if (odom_msg.twist.twist.linear.x != 0) or (odom_msg.twist.twist.linear.y != 0):
       odom_bag.write('odom_msg',odom_msg)
 
-def image_callback(img_msg):
+def depth_image_callback(depth_img_msg):
     global take_picture
     global num_nodes
     try:
         color_img = bridge.imgmsg_to_cv2(img_msg,desired_encoding='passthrough')
         if take_picture == True:
-            cv2.imwrite("/home/agilex/topological_mapping/images/image_" + str(num_nodes) + ".png", color_img)
+            cv2.imwrite("/home/administrator/mapping/images/depth_image_" + str(num_nodes) + ".png", color_img)
+            take_picture = False
+    except CvBridgeError as e:
+        print(e)
+
+def color_image_callback(color_img_msg):
+    global take_picture
+    global num_nodes
+    try:
+        color_img = bridge.imgmsg_to_cv2(img_msg,desired_encoding='passthrough')
+        if take_picture == True:
+            cv2.imwrite("/home/administrator/mapping/images/image_" + str(num_nodes) + ".png", color_img)
             take_picture = False
     except CvBridgeError as e:
         print(e)
@@ -46,11 +57,12 @@ q: quit the program
     num_saves = 1
     run = True
     rospy.init_node('graph_mapping', anonymous=True)
-    rospy.Subscriber("odom", Odometry, odom_callback)
-    rospy.Subscriber("camera/color/image_raw", Image, image_callback)
+    rospy.Subscriber("odometry/filtered", Odometry, odom_callback)
+    rospy.Subscriber("realsense_cam/color/image_raw", Image, color_image_callback)
+    rospy.Subscriber("realsense_cam/depth/image_rect_raw", Image, depth_image_callback)
     x_temp = x_pose
     y_temp = y_pose
-    graph_map.add_node(num_nodes, x_pose=x_temp, y_pose=y_temp, color_img="/home/agilex/topological_mapping/images/image_" + str(num_nodes) + ".png")
+    graph_map.add_node(num_nodes, x_pose=x_temp, y_pose=y_temp, color_img="/home/administrator/mapping/images/image_" + str(num_nodes) + ".png", depth_img="/home/administrator/mapping/images/depth_image_" + str(num_nodes) + ".png")
     take_picture = True
     print(msg)
     while(run):
@@ -59,7 +71,7 @@ q: quit the program
 	    num_nodes+=1
             x_temp = x_pose
             y_temp = y_pose
-            graph_map.add_node(num_nodes, x_pose=x_temp, y_pose=y_temp, color_img="/home/agilex/topological_mapping/images/image_" + str(num_nodes) + ".png")
+            graph_map.add_node(num_nodes, x_pose=x_temp, y_pose=y_temp, color_img="/home/administrator/mapping/images/image_" + str(num_nodes) + ".png", depth_img="/home/administrator/mapping/images/depth_image_" + str(num_nodes) + ".png")
             take_picture = True
             print('Number of nodes: ' + str(graph_map.number_of_nodes()))
         elif key == 'e':
@@ -74,12 +86,10 @@ q: quit the program
             print(graph_map.nodes(data=True))
             print(graph_map.edges(data=True))
         elif key == 's':
-            nx.write_graphml(graph_map, "/home/agilex/topological_mapping/maps/map"+str(num_saves)+".graphml", prettyprint=True)
+            nx.write_graphml(graph_map, "/home/administrator/mapping/maps/map"+str(num_saves)+".graphml", prettyprint=True)
             odom_bag.close()
         elif key == 'q':
             rospy.signal_shutdown('End of mapping.')
             run = False
         else:
             print('Please enter a valid command.\n')
-
-
